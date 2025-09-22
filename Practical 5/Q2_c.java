5dabd3109bd704ea8f48d44a97fd95a70ed76513
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 14:01:13 +0530

    Q2_c.java
    .java

---
+++ Practical 5/Q2_c.java
@@ -0,0 +1,9 @@
+//Q2 c Bike Class
+
+public class Bike extends Vehicle {
+    void display() {
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.println("This is a Bike");
+    }
+}
+
