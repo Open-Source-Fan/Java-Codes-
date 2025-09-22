c54b588f84946d08d245b787bd9c8b56118057c0
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 13:49:14 +0530

    Q1.java
    .java

---
+++ Practical 4/Q1.java
@@ -0,0 +1,15 @@
+//Experiment 4 Classes and Objects
+import java.util.Scanner;
+class Student {
+    String name;
+    int roll_no;
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "John";
+        s.roll_no = 2;
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.println("Name: " + s.name);
+        System.out.println("Roll No: " + s.roll_no);
+    }
+}
