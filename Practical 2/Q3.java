ce08f645b2ad523f7ab0811f9cfb473daaabd545
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 13:10:23 +0530

    Q3.java
    .java

---
+++ Practical 2/Q3.java
@@ -0,0 +1,20 @@
+//Q3. Print the right triangle
+import java.util.Scanner;
+class RightTrianglePrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            for(int j=i; j<n; j++) {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
