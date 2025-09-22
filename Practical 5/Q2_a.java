02928f89ce7f9287e0b2d574df83729aa2de4163
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 13:58:36 +0530

    Q2_a.java
    .java

---
+++ Practical 5/Q2_a.java
@@ -0,0 +1,9 @@
+//Q2. a) Vehicle example
+public class Vehicle {
+    void display() {
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.println("This is a Vehicle");
+    }
+}
+
+
