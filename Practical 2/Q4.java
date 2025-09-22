38dad4c4f4fe5be1bc7daa7bfc0dfea51574aa5a
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 13:11:13 +0530

    Q4.java
    .java

---
+++ Practical 2/Q4.java
@@ -0,0 +1,20 @@
+//Q4. Print the pyramid
+import java.util.Scanner;
+class PyramidPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++) {
+            for(int j=i; j<n; j++) {
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
