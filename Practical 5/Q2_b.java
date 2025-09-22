490fabaa57c066088c29cebdf5c3a1e7f942a346
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 13:59:26 +0530

    Q2_b.java
    .java

---
+++ Practical 5/Q2_b.java
@@ -0,0 +1,9 @@
+//Q2 b Car Class
+public class Car extends Vehicle {
+    void display() {
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.println("This is a Car");
+    }
+}
+
+
