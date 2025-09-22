cd56f1b9129cb67de74ddeddaa9ec70eeb52e70b
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-09-22 13:45:26 +0530

    Q7.java
    .java

---
+++ Practical 3/Q7.java
@@ -0,0 +1,33 @@
+//Q7. Reverse an Array
+import java.util.Scanner;
+public class ArrayReverse {
+    public static void main(String[] args) {
+    System.out.println("Shubhangi Tyagi 24csu294");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Original array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
