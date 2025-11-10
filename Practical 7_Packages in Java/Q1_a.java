1c439c1e56ecb880cfdd00a23d6af72246a37e2e
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-11-10 22:56:40 +0530

    Exp_7_Q1_aPackageDemo class in exercises.java package
    Created PackageDemo class with display method.

---
+++ Practical 7_Packages in Java/Q1_a.java
@@ -0,0 +1,13 @@
+//Exp7_Create a Java package called exercises. Inside the exercises package, create another package (subpackage) called java. Create a Java class called PackageDemo 
+//inside the java package. Insert a display() method inside the PackageDemo class. Inside the method, insert this statement:System.out.println("PackageDemo executed")
+//Write a PackageDemoDriver class within the same package to run the display () method of PackageDemo class
+//Soln 
+package exercises.java;        // package declaration
+
+public class PackageDemo {
+    public void display() {
+        System.out.println("Shubhangi Tyagi 24CSU294");
+        System.out.println("PackageDemo executed");
+    }
+}
+
