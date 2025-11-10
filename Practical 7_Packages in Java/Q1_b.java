fad03825626268b1576092a851b666c716542d20
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-11-10 22:58:05 +0530

    Exp_7_Q1_b.java

---
+++ Practical 7_Packages in Java/Q1_b.java
@@ -0,0 +1,10 @@
+//Exp7_part 2
+
+package exercises.java;        // same package
+
+public class PackageDemoDriver {
+    public static void main(String[] args) {
+        PackageDemo demo = new PackageDemo();
+        demo.display();
+    }
+}
