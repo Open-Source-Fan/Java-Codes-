e12764137f1acc0562cb7e27b29fea174ae87f97
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-11-11 22:13:29 +0530

    Q3_b.java

---
+++ Practical 8 Exceptions in Java/Q3_b.java
@@ -0,0 +1,6 @@
+//Exp8 part2 Exception inherits from MyBaseException
+public class MySecondLevelException extends MyBaseException {
+    public MySecondLevelException(String msg) {
+        super(msg);
+    }
+}
