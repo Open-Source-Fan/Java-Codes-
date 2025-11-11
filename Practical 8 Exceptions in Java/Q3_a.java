3b5c37bbe179ba6466143275b9071e184e53ec75
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-11-11 22:12:45 +0530

    Q3_a.java

---
+++ Practical 8 Exceptions in Java/Q3_a.java
@@ -0,0 +1,11 @@
+//Q3.	Create a three-level hierarchy of exceptions. Now create a base-class A with a method that throws an exception at the base of your hierarchy. 
+//Inherit B from A and override the method so it throws an exception at level two of your hierarchy. Repeat by inheriting class C from B. In main( ), 
+//create a C and upcast it to A, then call the method.
+
+//Exp8_part 1 Exception 
+public class MyBaseException extends Exception {
+    public MyBaseException(String msg) {
+        super(msg); // store the message in Exception class
+    }
+}
+
