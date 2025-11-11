7c58f5bf8607251751756e6986c9586cb32686f6
Open-Source-Fan <171322167+Open-Source-Fan@users.noreply.github.com>
2025-11-11 22:15:28 +0530

    Q3_d.java

---
+++ Practical 8 Exceptions in Java/Q3_d.java
@@ -0,0 +1,9 @@
+//Exp8 part4 class A
+public class A {
+    // Method that throws base-level exception
+    public void show() throws MyBaseException {
+        throw new MyBaseException("Exception from class A (Base level)");
+    }
+}
+
+
